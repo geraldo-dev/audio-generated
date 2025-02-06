7230ef0594e9fd87f9ceb074df198c818aa800b8
geraldo ferreira <geraldosousadev@gmail.com>
2025-02-06 14:00:04 -0300

    refactoring deck generator function

--- pack_generator.py
+++ pack_generator.py
@@ -1,57 +1,52 @@
-
 import genanki
 import os
+import csv
 
-# Cria um modelo de card básico com suporte a áudio
-modelo = genanki.Model(
-    1234567890,  # ID único
+phrase_en: list[str] = []
+phrase_pt: list[str] = []
+
+my_model = genanki.Model(
+    1234567890,
     'Modelo com Áudio',
     fields=[
-        {'name': 'Texto'},
+        {'name': 'front'},
+        {'name': 'back'},
         {'name': 'Audio'},
     ],
     templates=[
         {
             'name': 'Card de Frente',
-            'qfmt': '<h2>{{Texto}}</h2><br>{{Audio}}',  # Frente do cartão
-            # Resposta (inclui o áudio)
-            'afmt': '<h2>{{Texto}}</h2><br>{{Audio}}',
+            'qfmt': '<h2>{{front}}</h2><br>{{Audio}}',
+            'afmt': '<h2>{{front}}</h2>{{Audio}}<br><h3>{{back}}</h3>',
         },
     ],
+    css="h3, h2, br { text-align: center }"
 )
 
-# Cria um deck com um ID único
 deck = genanki.Deck(
-    123456789,  # ID único
+    123456789,
     'Deck com Áudio'
 )
 
-# Caminho para os arquivos de áudio
-pasta_audio = 'audio'
+folder_audio = 'sons'
 
-# Itera pelos arquivos de áudio na pasta
-for arquivo in os.listdir(pasta_audio):
-    if arquivo.endswith(('.mp3', '.wav')):
-        # Usa o nome do arquivo como texto
-        texto = os.path.splitext(arquivo)[0].replace('_', ' ')
-        # Formato esperado pelo Anki para áudio
-        caminho_audio = f'[sound:{arquivo}]'
+for file in os.listdir(folder_audio):
+    count: int = 0
+    if file.endswith(('.mp3', '.wav')):
 
-        # Cria uma nota (card)
+        path_audio = f'[sound:{file}]'
+
         nota = genanki.Note(
-            model=modelo,
-            fields=[texto, caminho_audio]
+            model=my_model,
+            fields=[str(phrase_en[count]), str(phrase_pt[count]), path_audio]
         )
-
-        # Adiciona a nota ao deck
+        count = count + 1
         deck.add_note(nota)
 
-# Cria um pacote e inclui os arquivos de áudio
-pacote = genanki.Package(deck)
-pacote.media_files = [os.path.join(pasta_audio, f) for f in os.listdir(
-    pasta_audio) if f.endswith(('.mp3', '.wav'))]
+pack = genanki.Package(deck)
+pack.media_files = [os.path.join(folder_audio, f) for f in os.listdir(
+    folder_audio) if f.endswith(('.mp3', '.wav'))]
 
-# Exporta o deck para um arquivo .apkg
-pacote.write_to_file('deck_com_audio.apkg')
+pack.write_to_file('deck_com_audio.apkg')
 
 print("Deck criado com sucesso: deck_com_audio.apkg")
